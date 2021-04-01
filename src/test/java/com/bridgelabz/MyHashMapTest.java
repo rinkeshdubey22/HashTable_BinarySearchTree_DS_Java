ab6c85e37fefbaf71dfce244fe40f074206f4207
Rinkesh Dubey <rinkeshdubey2@gmail.com>
2021-04-01 13:33:06 +0530

    [Rinkesh] : add Use LinkedList to do the Hash Table Opertaion Created MyMapNode with key Value Pair

---
+++ src/test/java/com/bridgelabz/MyHashMapTest.java
@@ -0,0 +1,31 @@
+package com.bridgelabz;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MyHashMapTest {
+
+    public static void beforeClass() throws Exception {
+
+        System.out.println("\n welcome to Data structure ");
+    }
+
+    @Test
+    public void givenASentenceWhenWordsAreAddedToListShouldReturnWordFrequency() {
+
+        String sentence = "To be or not to be";
+        MyHashMap <String, Integer> myHashMap = new MyHashMap<String, Integer>();
+        String[] words = sentence.toLowerCase().split(" ");
+        for(String word : words){
+            Integer value = myHashMap.get(word);
+            if(value == null) value = 1;
+            else value = value + 1;
+            myHashMap.add(word, value);
+        }
+        int frequency = myHashMap.get("to");
+        System.out.println(myHashMap);
+        Assertions.assertEquals(2, frequency);
+    }
+
+
+}
